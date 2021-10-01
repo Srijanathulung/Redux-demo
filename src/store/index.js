7b0679ec196f953578285edd1c070830ca60d6c4
Srijana Thulung <srijana.thulung123@gmail.com>
2021-10-01 10:21:13 +0545

    connecting with redux toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,28 @@
-import { createStore } from 'redux';
+// import { createStore } from 'redux';
+import { createSlice,configureStore } from '@reduxjs/toolkit';
 
 const initialState = { counter: 0, showCounter: true };
+
+const counterSlice=createSlice({
+    name: 'counter',
+    initialState: initialState,
+    reducers: {
+        increment(state) {
+            state.counter++;
+        },
+        decrement(state) {
+            state.counter--
+        },
+        increase(state, action) {
+            state.counter = state.counter + action.amount;
+         },
+        toggleCounter(state) {
+            state.showCounter = !state.showCounter;
+        }
+
+    }
+
+});
 
 const counterReducer = (state = initialState, action) => {
     if (action.type === 'increase') {
@@ -37,7 +59,9 @@ const counterReducer = (state = initialState, action) => {
     return state;
 };
 
-const store = createStore(counterReducer);
+const store = configureStore({
+    reducer:counterSlice.reducer
+});
 // console.log(store.getState());
 
 export default store;
