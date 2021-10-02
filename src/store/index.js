56a0c83955ff15c95927416625f39d0d195caff1
Srijana Thulung <srijana.thulung123@gmail.com>
2021-10-02 10:09:11 +0545

    splitting the code

--- src/store/index.js
+++ src/store/index.js
@@ -1,45 +1,7 @@
 // import { createStore } from 'redux';
-import { createSlice,configureStore } from '@reduxjs/toolkit';
-
-const initialCounterState = { counter: 0, showCounter: true }; 
-
-const counterSlice=createSlice({
-    name: 'counter',
-    initialState: initialCounterState,
-    reducers: {
-    
-        increment(state) {
-            state.counter++;
-        },
-        decrement(state) {
-            state.counter--
-        },
-        increase(state, action) {
-            state.counter = state.counter + action.payload;
-         },
-        toggleCounter(state) {
-            state.showCounter = !state.showCounter;
-        }
-
-    }
-
-});
-const initialAuthState = {
-    isAuthenticated:false
-};
-
-const authSlice=createSlice({
-    name: 'authentication',
-    initialState: initialAuthState,
-    reducers: {
-        login(state) {
-            state.isAuthenticated = true;
-         },
-        logout(state) {
-            state.isAuthenticated = false;
-        }
-    }
-});
+import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from './counter-slice';
+import authReducer from './auth-slice';
 
 // const counterReducer = (state = initialState, action) => {
 //     if (action.type === 'increase') {
@@ -77,11 +39,11 @@ const authSlice=createSlice({
 // };
 
 const store = configureStore({
-    reducer:{counter:counterSlice.reducer,auth:authSlice.reducer}
+    reducer:{counter:counterReducer,auth:authReducer}
 });
 
-export const counterActions = counterSlice.actions;
-export const authActions = authSlice.actions;
+// export const counterActions = counterSlice.actions;
+// export const authActions = authSlice.actions;
 
 
 // console.log(store.getState());
